7ed265ce2c39d88c1808132564473d11d4323d0b
victor01169 <victorroque01111@gmail.com>
2020-06-25 19:38:52 +0200

    Proyecto Final

--- src/java/Factory/MySQLConexionFactory.java
+++ src/java/Factory/MySQLConexionFactory.java
@@ -4,25 +4,28 @@ package Factory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public  final class MySQLConexionFactory extends ConexionDB {
+public final class MySQLConexionFactory extends ConexionDB{
 
-    
-    public MySQLConexionFactory(String[] criterios){
-    this.parametros = criterios;
-    this.open();
-    } 
-    
-    @Override 
-        public Connection open() {
-            try{
+    public MySQLConexionFactory(String[] criterios) {
+        //Pasar los criterios al array de la clase padre, parámetros.
+        this.parametros = criterios;
+        //Llamar al método open.
+        this.open();
+    }
+
+    //Implementar el método open.
+    @Override
+    Connection open() {
+        try{
             Class.forName("com.mysql.jdbc.Driver");
-            this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3307/" + this.parametros[0], this.parametros[1], this.parametros[2]);
-        } catch(Exception ex){
-            ex.printStackTrace();
+            //En this.parametros[] iran los datos respectivos de nombre de la BD, usuario y clave.
+            this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.parametros[0], this.parametros[1], this.parametros[2]);
+        }catch(Exception ex){
+          ex.printStackTrace();
         }
         return this.conexion;
-        
-        }
-       
+    }
+    
+
 }
 
